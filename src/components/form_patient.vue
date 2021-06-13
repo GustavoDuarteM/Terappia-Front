956b32b285dc698881b6d1d45b189fd714bcf969
Gustavo Duarte <Gustavo_dmuniz@hotmail.com>
2021-06-13 18:00:05 -0300

    Add form to add new patient

---
+++ src/components/form_patient.vue
@@ -0,0 +1,93 @@
+<template>
+  <v-dialog v-model="dialog" persistent width="80%">
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn
+        :color="btn_color"
+        :outlined="btn_outlined == 'true'"
+        v-bind="attrs"
+        v-on="on"
+      >
+        {{ btn_title }}
+      </v-btn>
+    </template>
+    <v-card>
+      <v-card-title>
+        <span class="text-h5">{{ form_title }}</span>
+      </v-card-title>
+      <v-card-text>
+        <v-text-field label="Nome" required v-model="name"></v-text-field>
+        <v-text-field label="Email" required v-model="email"></v-text-field>
+        <v-text-field label="Telefone" required v-model="phone"></v-text-field>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="blue darken-1" text @click="dialog = false">
+          Fechar
+        </v-btn>
+        <v-btn color="blue darken-1" text @click="save_patient"> Salvar </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    btn_title: {
+      required: true,
+    },
+    btn_color: {
+      type: String,
+      default: "primary",
+    },
+    btn_outlined: {
+      type: String,
+      default: "false",
+    },
+    patient: {
+      default: "",
+    },
+    update_list: {
+      required: true,
+    },
+    form_title: {
+      required: true,
+    },
+  },
+  data: () => ({
+    dialog: false,
+    name: "",
+    email: "",
+    phone: "",
+  }),
+  methods: {
+    save_patient: function () {
+      if(this.patient){
+        console.log('s')
+      }else{
+        this.new_patient()
+      }
+    },
+    new_patient: function () {
+      const params = {
+        patient: { name: this.name, email: this.email, phone: this.phone },
+      };
+      this.$http.auth
+        .post("/patients", params)
+        .then(() => {
+          this.dialog = false;
+          this.name = "";
+          this.email = "";
+          this.phone = "";
+          this.update_list();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- src/pages/Patients.vue
+++ src/pages/Patients.vue
@@ -1,28 +1,59 @@
 <template>
   <div>
-    <div v-for="patient in patients" :key="patient.id">
-      <PatientCard
-        :patient="patient"
-        class="ma-2"
-        :update_list="get_patients"
-      />
-    </div>
+    <v-card class="ma-3">
+      <v-card-title>
+        Lista de Pacientes
+        <v-spacer></v-spacer>
+        <v-text-field
+          label="Buscar Paciente"
+          single-line
+          append-icon="mdi-magnify"
+          v-model="name"
+        ></v-text-field>
+        <v-spacer></v-spacer>
+        <FormPatient
+          form_title="Novo Paciente"
+          btn_title="Adicionar Paciente"
+          btn_color="green"
+          btn_outlined="true"
+          :update_list="get_patients"
+        />
+      </v-card-title>
+    </v-card>
+    <template v-if="patients">
+      <div v-for="patient in patients" :key="patient.id">
+        <PatientCard
+          :patient="patient"
+          class="ma-2"
+          :update_list="get_patients"
+        />
+      </div>
+    </template>
+    <template v-else>
+      <ListLoading />
+    </template>
   </div>
 </template>
 
 <script>
 import PatientCard from "../components/patient_card.vue";
+import ListLoading from "../components/list_loading.vue";
+import FormPatient from "../components/form_patient.vue";
 export default {
   components: {
     PatientCard,
+    ListLoading,
+    FormPatient,
   },
   data: () => ({
-    patients: [],
+    patients: "",
+    name:""
   }),
   methods: {
     get_patients: function () {
+      this.patients = "";
       this.$http.auth
-        .get("/patients")
+        .get("/patients",{ params: {name: this.name}})
         .then((response) => {
           this.patients = response.data.patients;
         })
@@ -30,6 +61,17 @@ export default {
           console.log(error);
         });
     },
+  },
+  watch:{
+    name(val){
+      if(val.length > 3){
+        this.patients = "";
+        this.get_patients()
+      }else if(val.length == 0){
+        this.patients = "";
+        this.get_patients()
+      }
+    }
   },
   beforeMount() {
     this.get_patients();
