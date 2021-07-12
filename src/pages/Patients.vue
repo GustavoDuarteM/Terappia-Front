fec7ca3c97d3682f762b3fa8449b1e0f03c1f966
Gustavo Duarte <Gustavo_dmuniz@hotmail.com>
2021-07-11 22:25:12 -0300

    Add pagination in Paients

--- src/pages/Patients.vue
+++ src/pages/Patients.vue
@@ -21,13 +21,20 @@
       </v-card-title>
     </v-card>
     <template v-if="patients">
-      <div v-for="patient in patients" :key="patient.id">
-        <PatientCard
-          :patient="patient"
-          class="ma-2"
-          :update_list="get_patients"
-        />
+      <div>
+        <div v-for="patient in patients" :key="patient.id">
+          <PatientCard
+            :patient="patient"
+            class="ma-2"
+            :update_list="get_patients"
+          />
+        </div>
       </div>
+            <Paginator
+        :set_page="set_page"
+        :has_next="has_next"
+        :has_prev="has_prev"
+      />
     </template>
     <template v-else>
       <ListLoading />
@@ -39,39 +46,57 @@
 import PatientCard from "../components/patient_card.vue";
 import ListLoading from "../components/list_loading.vue";
 import FormPatient from "../components/form_patient.vue";
+import Paginator from "../components/paginator.vue";
+
 export default {
   components: {
     PatientCard,
     ListLoading,
     FormPatient,
+    Paginator,
   },
   data: () => ({
     patients: "",
-    name:""
+    name: "",
+    page: 1,
+    has_next: false,
+    has_prev: false,
   }),
   methods: {
     get_patients: function () {
       this.patients = "";
       this.$http.auth
-        .get("/patients",{ params: {name: this.name}})
+        .get("/patients", { params: { name: this.name , page: this.page} })
         .then((response) => {
+          this.has_next = response.data.has_next;
+          this.has_prev = response.data.has_prev;
           this.patients = response.data.patients;
-        })
-        .catch((error) => {
-          console.log(error);
         });
     },
-  },
-  watch:{
-    name(val){
-      if(val.length > 3){
-        this.patients = "";
-        this.get_patients()
-      }else if(val.length == 0){
-        this.patients = "";
-        this.get_patients()
+    set_page: function (step){
+      switch (step) {
+        case 'next':
+          this.page++;
+        break;
+        case 'prev':
+          if (this.page > 1){
+            this.page--;
+          }
+        break;
       }
+      this.get_patients()
     }
+  },
+  watch: {
+    name(val) {
+      if (val.length > 3) {
+        this.patients = "";
+        this.get_patients();
+      } else if (val.length == 0) {
+        this.patients = "";
+        this.get_patients();
+      }
+    },
   },
   beforeMount() {
     this.get_patients();
